e712f1e654bc9d620b4dfd09dcf0de04b4eab724
sk <content.type.text.net@gmail.com>
2024-04-21 20:30:08 +0900

    test?

---
+++ Source/Pe.Installer.Test/ListItemTest.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pe.Installer.Test
+{
+    public class ListItemTest_String
+    {
+        [Theory]
+        [InlineData("display", "value")]
+        public void ConstructorTest(string display, string value) {
+            var test = new ListItem<string>(display, value);
+            Assert.Equal(display, test.Display);
+            Assert.Equal(value, test.Value);
+        }
+    }
+}
