1631dca0e1014e5ccafb1a1492db36ebbf107389
sk <content.type.text.net@gmail.com>
2024-12-02 19:29:35 +0900

    できたー？

--- Source/Pe.Installer.Test/DownloaderTest.cs
+++ Source/Pe.Installer.Test/DownloaderTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -29,12 +30,32 @@ namespace Pe.Installer.Test
                 )
                 .ReturnsAsync(new HttpResponseMessage() {
                     StatusCode = HttpStatusCode.OK,
-                    Content = new StringContent("[{'key':123}]"),
+                    Content = new StringContent(@"{
+                        ""items"": [
+                            {
+                                ""version"": ""1.20.003"",
+                                ""minimum_version"": ""0.91.000"",
+                                ""release"": ""2345-12-31T00:00:00Z"",
+                                ""revision"": ""REVISION"",
+                                ""platform"": ""x86"",
+                                ""note_uri"": ""http://localhost.invalid/note"",
+                                ""archive_uri"": ""http://localhost.invalid/archive"",
+                                ""archive_size"": 9223372036854775807,
+                                ""archive_kind"": ""7z"",
+                                ""archive_hash_kind"": ""SHA256"",
+                                ""archive_hash_value"": ""ARCHIVE_HASH_VALUE""
+                            }
+                        ]
+                    }"),
                 })
                 .Verifiable()
             ;
             var progressLoggerMock = new Mock<IProgressLogger>();
             var loggerFactoryMock = new Mock<ILoggerFactory>();
+            loggerFactoryMock
+                .Setup((m) => m.CreateLogger(It.IsAny<Type>()))
+                .Returns(new Mock<ILogger>().Object)
+            ;
 
 
             var downloader = new Downloader(
@@ -42,8 +63,20 @@ namespace Pe.Installer.Test
                 progressLoggerMock.Object,
                 loggerFactoryMock.Object
             );
-            await downloader.GetUpdateItemDataAsync(new Uri("/update"), "x86", CancellationToken.None);
 
+            var actual_x86 = await downloader.GetUpdateItemDataAsync(new Uri("http://localhost.invalid/update"), "x86", CancellationToken.None);
+
+            Assert.Equal(new Version("1.20.003"), actual_x86.Version);
+            Assert.Equal(new Version("0.91.000"), actual_x86.MinimumVersion);
+            Assert.Equal(DateTimeOffset.Parse("2345-12-31T00:00:00Z", CultureInfo.InvariantCulture), actual_x86.Release);
+            Assert.Equal("REVISION", actual_x86.Revision);
+            Assert.Equal("x86", actual_x86.Platform);
+            Assert.Equal(new Uri("http://localhost.invalid/note"), actual_x86.NoteUri);
+            Assert.Equal(new Uri("http://localhost.invalid/archive"), actual_x86.ArchiveUri);
+            Assert.Equal(9223372036854775807L, actual_x86.ArchiveSize);
+            Assert.Equal("7z", actual_x86.ArchiveKind);
+            Assert.Equal("SHA256", actual_x86.ArchiveHashKind);
+            Assert.Equal("ARCHIVE_HASH_VALUE", actual_x86.ArchiveHashValue);
         }
 
         #endregion
